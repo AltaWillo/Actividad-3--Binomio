7a50e8a8f0eb8faa6a582d7380620617f81b71b8
Alumno <Alumno@Departamento-01>
2017-08-21 08:38:11 -0500

    Elevar binomio al cuadrado y al cubo.

---
+++ src/binomio/Binomio.java
@@ -0,0 +1,17 @@
+
+package binomio;
+
+public class Binomio {
+
+    
+    public static void main(String[] args) {
+       
+        Binomioo bi=new Binomioo();
+        bi.setVisible(true);
+        int a, b;
+        System.out.println("(a+b)^2 = a^2+ 2ab + b^2");
+        
+    }
+    
+    
+}
